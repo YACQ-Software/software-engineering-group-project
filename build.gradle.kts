d0dee2df934272dd9d5e1e54cbcfd8fda556088e
Christian-Chapajong <chapchris04@gmail.com>
2025-05-02 14:04:58 +0100

    CC - tailwind added through CDN

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,19 +17,22 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
-    implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
-    implementation("org.springframework.boot:spring-boot-starter-data-rest")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("edu.stanford.nlp:stanford-corenlp:4.5.4")
-    implementation("edu.stanford.nlp:stanford-corenlp:4.5.4:models") {
-        exclude(group = "edu.stanford.nlp", module = "stanford-corenlp")
-    }
-    compileOnly("org.projectlombok:lombok")
-    developmentOnly("org.springframework.boot:spring-boot-devtools")
-    annotationProcessor("org.projectlombok:lombok")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-data-rest")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("edu.stanford.nlp:stanford-corenlp:4.5.4")
+	implementation("edu.stanford.nlp:stanford-corenlp:4.5.4:models") {
+		exclude(group = "edu.stanford.nlp", module = "stanford-corenlp") // Avoid duplicate classes
+	}
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.projectlombok:lombok")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.0") // or latest
+	testImplementation("org.mockito:mockito-core:5.12.0") // or latest
+	testImplementation("org.mockito:mockito-junit-jupiter:5.12.0")
 }
 
 tasks.withType<Test> {
